615ce22a656a5673e4cbe340e8b3d8bf6c37cf68
whartsell <will@willshouse.net>
2016-08-17 15:18:55 -0500

    added F-15, Mirage, and template

---
+++ Config/Input/L-39C/joystick/Thrustmaster Combined {DCDA60C0-CD13-11e5-8002-444553540000}.diff.lua
@@ -0,0 +1,21 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2003cdnil"] = {
+			["name"] = "Rudder",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+		["a2012cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER2",
+				},
+			},
+			["name"] = "Zoom View",
+		},
+	},
+}
+return diff

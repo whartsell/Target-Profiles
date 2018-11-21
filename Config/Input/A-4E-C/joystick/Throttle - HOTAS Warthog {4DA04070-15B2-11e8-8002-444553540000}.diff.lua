f25ba5d5ee06bc7820913822924353e218979aa3
whartsell <will@willshouse.net>
2018-11-21 09:34:29 -0800

    added A-4

---
+++ Config/Input/A-4E-C/joystick/Throttle - HOTAS Warthog {4DA04070-15B2-11e8-8002-444553540000}.diff.lua
@@ -0,0 +1,196 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2004cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+			["name"] = "Thrust",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d10003pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "Spoilers ARM-OFF Toggle",
+		},
+		["d10004pnilu10005cdnilvdnilvpnilvunil"] = {
+			["name"] = "*Spoilers ARM: ON else OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+		},
+		["d10026pnilu10027cdnilvdnilvpnilvunil"] = {
+			["name"] = "*Fuel Gauge EXT else INT",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN32",
+				},
+			},
+		},
+		["d10069pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Radar Mode: Standby",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["d10074pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Radar Mode CW",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+		["d10075pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Radar Mode CCW",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["d10076pnilu10076cdnilvd1vpnilvu0"] = {
+			["name"] = "*Radar Terrain Clearance: Plan ELSE Profile",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+		},
+		["d10077pnilu10077cdnilvd1vpnilvu0"] = {
+			["name"] = "*Radar Range: Long ELSE Short",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+		},
+		["d10093pnilu10093cdnilvd-1vpnilvu0"] = {
+			["name"] = "*Master Exterior Lights: BRIEF else OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+		},
+		["d10093pnilu10093cdnilvd1vpnilvu0"] = {
+			["name"] = "*Master Exterior Lights: ON else OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+		},
+		["d10093pnilunilcdnilvd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Master Exterior Lights: OFF",
+		},
+		["d10093pnilunilcdnilvd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Master Exterior Lights: ON",
+		},
+		["d10123pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "AFCS Override (emergency)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+		},
+		["d10130pnilu10130cdnilvd-1vpnilvu0"] = {
+			["name"] = "*AFCS Altitude Mode else Altitude+Heading (HOTAS)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+		},
+		["d10130pnilu10130cdnilvd1vpnilvu0"] = {
+			["name"] = "*AFCS Path Mode else Altitude+Heading (HOTAS)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+		},
+		["d10134pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "*AFCS Engage (HOTAS)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+		},
+		["d147pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Airbrake On",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+		},
+		["d148pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Airbrake Off",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+		},
+		["d68pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN21",
+				},
+			},
+			["name"] = "Landing Gear Up/Down",
+		},
+		["d69pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "Tail Hook Up/Down",
+		},
+		["d73pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Airbrake",
+		},
+		["d957pnilu958cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "T/O Trim",
+		},
+	},
+}
+return diff

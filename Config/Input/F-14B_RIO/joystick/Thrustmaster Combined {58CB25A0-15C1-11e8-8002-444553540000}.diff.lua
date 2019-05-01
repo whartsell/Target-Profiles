5533326f8d3d38467fa5ad9f6769ada6061694c3
whartsell <will@willshouse.net>
2019-05-01 11:14:47 -0700

    rio changes
    also WIP LANTIRN

---
+++ Config/Input/F-14B_RIO/joystick/Thrustmaster Combined {58CB25A0-15C1-11e8-8002-444553540000}.diff.lua
@@ -0,0 +1,225 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3091pnilu3091cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "HCU half-action",
+		},
+		["d3092pnilu3092cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "HCU full-action",
+		},
+		["d3094pnilu3094cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+			["name"] = "HCU offset",
+		},
+		["d3095pnilu3095cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "HCU MRL",
+		},
+		["d3096pnilu3096cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN11",
+				},
+			},
+			["name"] = "HCU TCS mode",
+		},
+		["d3097pnilu3097cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "HCU radar mode",
+		},
+		["d3098pnilu3098cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "HCU DDD mode",
+		},
+		["d3099pnilu3099cd58vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "HCU TID mode",
+		},
+		["d3105pnilu3105cd39vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "HCU thumb down",
+		},
+		["d3105pnilu3105cd39vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "HCU thumb up",
+		},
+		["d3269pnilu3269cd5vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN21",
+				},
+			},
+			["name"] = "AN/ALE-39 Chaff SGL",
+		},
+		["d3270pnilu3270cd5vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN23",
+				},
+			},
+			["name"] = "AN/ALE-39 Flare SGL",
+		},
+		["d3444pnilu3444cd39vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+			["name"] = "Radar elevation down",
+		},
+		["d3444pnilu3444cd39vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "Radar elevation up",
+		},
+		["d3445pnilu3445cd39vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "Radar azimuth left",
+		},
+		["d3445pnilu3445cd39vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Radar azimuth right",
+		},
+		["d3448pnilu3448cd39vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN20",
+				},
+			},
+			["name"] = "VSL Low",
+		},
+		["d3448pnilu3448cd39vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+				},
+			},
+			["name"] = "VSL High",
+		},
+		["d3464pnilu3464cd39vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "DDD Interrogate Friend or Foe",
+		},
+		["d3494pnilunilcd37vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "TCS FOV narrow",
+		},
+		["d3494pnilunilcd37vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+				},
+			},
+			["name"] = "TCS FOV wide",
+		},
+		["d3667pnilu3667cd39vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "HCU thumb re-center",
+		},
+		["d3682pnilu3682cd38vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "LANTIRN Slew Left",
+		},
+		["d3682pnilu3682cd38vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "LANTIRN Slew Right",
+		},
+		["d3683pnilu3683cd38vd-1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "LANTIRN Slew Down",
+		},
+		["d3683pnilu3683cd38vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "LANTIRN Slew Up",
+		},
+	},
+}
+return diff

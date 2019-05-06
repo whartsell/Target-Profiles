edeb7e179e0224589271426293daf1cac31448b7
whartsell <will@willshouse.net>
2019-05-05 21:29:27 -0700

    updates to f-86 and F-18

--- Config/Input/F-14B/joystick/Joystick - HOTAS Warthog {4DA04070-15B2-11e8-8003-444553540000}.diff.lua
+++ Config/Input/F-14B/joystick/Joystick - HOTAS Warthog {4DA04070-15B2-11e8-8003-444553540000}.diff.lua
@@ -1,11 +1,96 @@
 local diff = {
-	["keyDiffs"] = {
-		["d3143pnilu3143cd55vd1vpnilvu0"] = {
-			["added"] = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["name"] = "Pitch",
+			["removed"] = {
 				[1] = {
-					["key"] = "JOY_BTN12",
+					["key"] = "JOY_Y",
 				},
 			},
+		},
+		["a2002cdnil"] = {
+			["name"] = "Roll",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_X",
+				},
+			},
+		},
+	},
+	["keyDiffs"] = {
+		["d3078pnilu3078cd57vd1vpnilvu0"] = {
+			["name"] = "Store Release",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+		},
+		["d3080pnilu3080cd57vd1vpnilvu0"] = {
+			["name"] = "Weapon Selector up",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+		},
+		["d3081pnilu3081cd57vd1vpnilvu0"] = {
+			["name"] = "Weapon Selector down",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN9",
+				},
+			},
+		},
+		["d3082pnilu3082cd57vd1vpnilvu0"] = {
+			["name"] = "Weapon Selector press",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+		},
+		["d3084pnilu3084cd57vd1vpnilvu0"] = {
+			["name"] = "DLC Toggle / Countermeasure Dispense",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN19",
+				},
+			},
+		},
+		["d3085pnilu3085cd57vd1vpnilvu0"] = {
+			["name"] = "Autopilot Reference / Nosewheel Steering Toggle",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+		},
+		["d3086pnilu3086cd21vd1vpnilvu0"] = {
+			["name"] = "Autopilot Emergency Disconnect Paddle",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+		},
+		["d3087pnilu3087cd57vd1vpnilvu0"] = {
+			["name"] = "Trigger",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+		},
+		["d3088pnilu3088cd57vd1vpnilvu0"] = {
+			["name"] = "Trigger First Detent",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+		},
+		["d3143pnilu3143cd55vd1vpnilvu0"] = {
 			["name"] = "Target designate up / VSL HI",
 			["removed"] = {
 				[1] = {
@@ -14,11 +99,6 @@ local diff = {
 			},
 		},
 		["d3144pnilu3144cd55vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN11",
-				},
-			},
 			["name"] = "Target designate fwd / PAL",
 			["removed"] = {
 				[1] = {
@@ -27,11 +107,6 @@ local diff = {
 			},
 		},
 		["d3145pnilu3145cd55vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN14",
-				},
-			},
 			["name"] = "Target designate down / VSL LO",
 			["removed"] = {
 				[1] = {
@@ -39,13 +114,53 @@ local diff = {
 				},
 			},
 		},
-		["d3550pnilu3550cd62vd1vpnilvu-1"] = {
-			["added"] = {
+		["dnilp2019u2019cdnilvdnilvp-1vu0"] = {
+			["name"] = "Trim Pitch Down",
+			["removed"] = {
 				[1] = {
-					["key"] = "JOY_BTN5",
+					["key"] = "JOY_BTN_POV1_U",
 				},
 			},
-			["name"] = "Toggle Menu",
+		},
+		["dnilp2019u2019cdnilvdnilvp1vu0"] = {
+			["name"] = "Trim Pitch Up",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+		["dnilp2020u2020cdnilvdnilvp-1vu0"] = {
+			["name"] = "Trim Roll Left Wing Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp2020u2020cdnilvdnilvp1vu0"] = {
+			["name"] = "Trim Roll Right Wing Down",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp3083u3083cd57vdnilvp-1vu0"] = {
+			["name"] = "DLC Thumbwheel Aft",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+		},
+		["dnilp3083u3083cd57vdnilvp1vu0"] = {
+			["name"] = "DLC Thumbwheel Forward",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
 		},
 	},
 }
---
+++ Config/Input/F-14B_RIO/joystick/Throttle - HOTAS Warthog {4DA04070-15B2-11e8-8002-444553540000}.diff.lua
@@ -0,0 +1,31 @@
+local diff = {
+	["axisDiffs"] = {
+		["a2012cdnil"] = {
+			["name"] = "Zoom View",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+		},
+		["a3668cd39"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_SLIDER1",
+				},
+			},
+			["name"] = "HCU vernier thumbwheel relative adjustment",
+		},
+	},
+}
+return diff

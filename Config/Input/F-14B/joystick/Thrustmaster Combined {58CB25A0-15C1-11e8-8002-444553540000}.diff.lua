32fbfed4de318f177a3a7abc687a2a4e0e20d49e
whartsell <will@willshouse.net>
2019-05-30 22:06:39 -0700

    f14 commit

--- Config/Input/F-14B/joystick/Thrustmaster Combined {58CB25A0-15C1-11e8-8002-444553540000}.diff.lua
+++ Config/Input/F-14B/joystick/Thrustmaster Combined {58CB25A0-15C1-11e8-8002-444553540000}.diff.lua
@@ -1,41 +1,5 @@
 local diff = {
 	["axisDiffs"] = {
-		["a2001cdnil"] = {
-			["changed"] = {
-				[1] = {
-					["filter"] = {
-						["curvature"] = {
-							[1] = 0,
-						},
-						["deadzone"] = 0.02,
-						["invert"] = false,
-						["saturationX"] = 1,
-						["saturationY"] = 1,
-						["slider"] = false,
-					},
-					["key"] = "JOY_Y",
-				},
-			},
-			["name"] = "Pitch",
-		},
-		["a2002cdnil"] = {
-			["changed"] = {
-				[1] = {
-					["filter"] = {
-						["curvature"] = {
-							[1] = 0,
-						},
-						["deadzone"] = 0.02,
-						["invert"] = false,
-						["saturationX"] = 1,
-						["saturationY"] = 1,
-						["slider"] = false,
-					},
-					["key"] = "JOY_X",
-				},
-			},
-			["name"] = "Roll",
-		},
 		["a2003cdnil"] = {
 			["name"] = "Rudder",
 			["removed"] = {

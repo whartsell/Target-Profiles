fc55070a56485535420d769f39e0869c3af96c7c
whartsell <will@willshouse.net>
2019-05-30 21:23:33 -0700

    initial commit
    initial commit

---
+++ VPC/DCS/F-5E-3/joystick/Throttle - HOTAS Warthog {4DA04070-15B2-11e8-8002-444553540000}.diff.lua
@@ -0,0 +1,266 @@
+local diff = {
+	["axisDiffs"] = {
+		["a3002cd17"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.1,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 0.6,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "AN/APQ-159 Radar TDC Button - Azimuth",
+		},
+		["a3003cd17"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0.1,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 0.6,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "AN/APQ-159 Radar TDC Button - Range",
+		},
+	},
+	["keyDiffs"] = {
+		["d3001pnilu3001cd24vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN4",
+				},
+			},
+			["name"] = "UHF Radio Microphone Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN3",
+				},
+			},
+		},
+		["d3001pnilu3001cd2vd1vpnilvu0"] = {
+			["name"] = "Yaw Damper Switch (HOTAS)- YAW/OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+		},
+		["d3002pnilu3002cd2vd1vpnilvu0"] = {
+			["name"] = "Pitch Damper Switch (HOTAS)- PITCH/OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN24",
+				},
+			},
+		},
+		["d3004pnilunilcd11vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Landing & Taxi Light Switch - OFF",
+		},
+		["d3004pnilunilcd11vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN13",
+				},
+			},
+			["name"] = "Landing & Taxi Light Switch - ON",
+		},
+		["d3007pnilu3007cd13vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "Flare-Chaff Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+		},
+		["d3007pnilu3007cd18vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN12",
+				},
+			},
+			["name"] = "AN/ASG-31 Sight Cage Switch",
+		},
+		["d3007pnilu3007cd7vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN21",
+				},
+			},
+			["name"] = "Landing Gear and Flap Warning Silence Button",
+		},
+		["d3008pnilunilcd18vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+			["name"] = "AN/ASG-31 Sight Mode Selector - CCW/Left",
+		},
+		["d3008pnilunilcd18vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+			["name"] = "AN/ASG-31 Sight Mode Selector - CW/Right",
+		},
+		["d3011pnilu3011cd15vd-1vpnilvu0"] = {
+			["name"] = "Guns, Missile and Camera Switch - CAMR ONLY/OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN28",
+				},
+			},
+		},
+		["d3011pnilu3011cd15vd1vpnilvu0"] = {
+			["name"] = "Guns, Missile and Camera Switch - GUNS MSL & CAMR/OFF",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN27",
+				},
+			},
+		},
+		["d3013pnilunilcd17vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+			["name"] = "AN/APQ-159 Radar Range Selector - CCW/Decrease",
+		},
+		["d3013pnilunilcd17vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+			["name"] = "AN/APQ-159 Radar Range Selector - CW/Increase",
+		},
+		["d3021pnilunilcd2vd1vpnilvunil"] = {
+			["name"] = "Throttle Range (Press to change)",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN21",
+				},
+			},
+		},
+		["d509pnilu510cdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "AN/APQ-159 Radar ACQ Button",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+		},
+		["d68pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+			["name"] = "Landing Gear Lever - LG UP/LG DOWN",
+		},
+		["d76pnilu76cdnilvd1vpnilvu0"] = {
+			["name"] = "Drag Chute T-Handle - PULL/PUSH",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN26",
+				},
+			},
+		},
+		["dnilp210u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UR",
+				},
+			},
+		},
+		["dnilp211u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DR",
+				},
+			},
+		},
+		["dnilp212u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_DL",
+				},
+			},
+		},
+		["dnilp213u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_UL",
+				},
+			},
+		},
+		["dnilp32u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Left slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_L",
+				},
+			},
+		},
+		["dnilp33u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Right slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_R",
+				},
+			},
+		},
+		["dnilp34u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Up slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_U",
+				},
+			},
+		},
+		["dnilp35u214cdnilvdnilvpnilvunil"] = {
+			["name"] = "View Down slow",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_BTN_POV1_D",
+				},
+			},
+		},
+	},
+}
+return diff
